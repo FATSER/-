13e9a0fab1d8de86814b30fd73af23036c0a2a4a
faster <1690517991@qq.com>
2019-05-27 18:52:13 +0800

    第二次上传

--- app/src/main/java/com/example/test2o4/MapActivity.java
+++ app/src/main/java/com/example/test2o4/MapActivity.java
@@ -40,20 +40,10 @@ public class MapActivity extends AppCompatActivity {
         SDKInitializer.initialize(getApplicationContext());
         SDKInitializer.initialize( MapActivity.this );
         setContentView(R.layout.mapactivity);
-
         mapView= (MapView) findViewById(R.id.mapview);
         baiduMap = mapView.getMap();
-
         baiduMap.setMyLocationEnabled(true);//开启定位图层
-
-        //用户自定义定位图标
         BitmapDescriptor mCurrentMarker = BitmapDescriptorFactory.fromResource(R.mipmap.icon_geo);
-
-        //参数1: 有三个值 LocationMode.COMPASS 罗盘态，显示定位方向圈，保持定位图标在地图中心
-        //              LocationMode.FOLLOWING 跟随态，保持定位图标在地图中心
-        //              LocationMode.NORMAL  普通态： 更新定位数据时不对地图做任何操作
-        //参数2:是否允许显示方向信息
-        //参数3:用户自定义定位图标
         MyLocationConfiguration config = new MyLocationConfiguration(MyLocationConfiguration.LocationMode.NORMAL,true,mCurrentMarker);
         baiduMap.setMyLocationConfiguration(config);//设置定位图层显示方式
 
@@ -97,12 +87,8 @@ public class MapActivity extends AppCompatActivity {
                 StringBuffer sb = new StringBuffer(256);
                 Log.i("ansen","getLongitude:"+location.getLongitude());
                 sb.append("当前定位时间 : ");
-                /**
-                 * 时间也可以使用systemClock.elapsedRealtime()方法 获取的是自从开机以来，每次回调的时间；
-                 * location.getTime() 是指服务端出本次结果的时间，如果位置不发生变化，则时间不变
-                 */
                 sb.append(location.getTime());
-                sb.append("\n定位类型 : ");// 定位类型
+                sb.append("\n定位类型 : ");
                 if(location.getLocType()==61){
                     sb.append("GPS定位成功");
                 }else if (location.getLocType()==161){
@@ -114,50 +100,35 @@ public class MapActivity extends AppCompatActivity {
                 }else if (location.getLocType()==67){
                     sb.append("离线定位失败");
                 }else sb.append(location.getLocType());
-                //sb.append("\n定位类型说明 : ");// *****对应的定位类型说明*****
-                // sb.append(location.getLocTypeDescription());
-                sb.append("\n纬度 : ");// 纬度
+                sb.append("\n纬度 : ");
                 sb.append(location.getLatitude());
-                sb.append("   经度 : ");// 经度
+                sb.append("   经度 : ");
                 sb.append(location.getLongitude());
-                sb.append("   精度半径 : ");// 半径
+                sb.append("   精度半径 : ");
                 sb.append(location.getRadius()+"米");
-                //sb.append("\n国家码 : ");// 国家码
-                //sb.append(location.getCountryCode());
-                //sb.append("   城市编码 : ");// 城市编码
-                //sb.append(location.getCityCode());
-                //sb.append("\n国家名称 : ");// 国家名称
-                //sb.append(location.getCountry());
-                //sb.append("\n城市 : ");// 城市
-                //sb.append(location.getCity());
-                //sb.append("\n区 : ");// 区
-                //sb.append(location.getDistrict());
-                //sb.append("\n街道 : ");// 街道
-                //sb.append(location.getStreet());
+
                 sb.append("\n罗盘方位: ");
-                sb.append(location.getDirection());// 方向
-                sb.append("\n地址信息 : ");// 地址信息
+                sb.append(location.getDirection());
+                sb.append("\n地址信息 : ");
                 sb.append(location.getAddrStr());
-                //sb.append("\nUserIndoorState: ");// *****返回用户室内外判断结果*****
-                //sb.append(location.getUserIndoorState());
+
                 sb.append("\n位置描述: ");
-                sb.append(location.getLocationDescribe());// 位置语义化信息
-                sb.append("\n附近信息: ");// POI信息
+                sb.append(location.getLocationDescribe());
+                sb.append("\n附近信息: ");
                 if (location.getPoiList() != null && !location.getPoiList().isEmpty()) {
                     for (int i = 0; i < location.getPoiList().size(); i++) {
                         Poi poi = (Poi) location.getPoiList().get(i);
                         sb.append(poi.getName() + ";\n");
                     }
                 }
-                if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+                if (location.getLocType() == BDLocation.TypeGpsLocation) {
                     sb.append("\n速度 : ");
-                    sb.append(location.getSpeed()+"km/h");// 速度 单位：km/h
+                    sb.append(location.getSpeed()+"km/h");
                     sb.append("\n海拔 : ");
-                    sb.append(location.getAltitude()+"米");// 海拔高度 单位：米
+                    sb.append(location.getAltitude()+"米");
                     sb.append("\n卫星数目 : ");
-                    sb.append(location.getSatelliteNumber()+"颗");// 卫星数目
+                    sb.append(location.getSatelliteNumber()+"颗");
                     sb.append("\nGPS质量 : ");
-                    //sb.append(location.getGpsAccuracyStatus());// *****gps质量判断*****
                     if (location.getGpsAccuracyStatus()==3){
                         sb.append("弱 ");
                     }else if (location.getGpsAccuracyStatus()==2){
@@ -168,14 +139,12 @@ public class MapActivity extends AppCompatActivity {
                     }
                     sb.append("\n描述 : ");
                     sb.append("gps定位成功");
-                } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
-                    // 运营商信息
-                    if (location.hasAltitude()) {// *****如果有海拔高度*****
+                } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {
+                    if (location.hasAltitude()) {
                         sb.append("\n海拔 : ");
-                        sb.append(location.getAltitude()+"米");// 单位：米
+                        sb.append(location.getAltitude()+"米");
                     }
-                    sb.append("\n运营商信息 : ");// 运营商信息
-                    //sb.append(location.getOperators());
+                    sb.append("\n运营商信息 : ");
                     if(location.getOperators()==1){
                         sb.append("中国移动");
                     }
@@ -187,7 +156,7 @@ public class MapActivity extends AppCompatActivity {
                     }
                     sb.append("\n描述 : ");
                     sb.append("网络定位成功");
-                } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+                } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {
                     sb.append("\n描述 : ");
                     sb.append("离线定位成功，离线定位结果也是有效的");
                 } else if (location.getLocType() == BDLocation.TypeServerError) {
@@ -202,20 +171,17 @@ public class MapActivity extends AppCompatActivity {
                 }
                 tvLocationResult.setText(sb.toString());
                 MyLocationData locData = new MyLocationData.Builder()
-                        .accuracy(location.getRadius())//设置定位数据的精度信息
-                        .latitude(location.getLatitude())//纬度
-                        .longitude(location.getLongitude())//经度
+                        .accuracy(location.getRadius())
+                        .latitude(location.getLatitude())
+                        .longitude(location.getLongitude())
                         .build();
-
-                //设置定位数据, 只有先允许定位图层后设置数据才会生效
                 baiduMap.setMyLocationData(locData);
 
-                if (isFirstLoc) {//第一次定位
+                if (isFirstLoc) {
                     isFirstLoc = false;
                     LatLng ll = new LatLng(location.getLatitude(),location.getLongitude());
                     MapStatus.Builder builder = new MapStatus.Builder();
-                    builder.target(ll).zoom(18.0f);//设置地图缩放级别
-                    //以动画方式更新地图状态，动画耗时 300 ms
+                    builder.target(ll).zoom(18.0f);
                     baiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
                 }
             }
@@ -237,14 +203,12 @@ public class MapActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        //在activity执行onPause时执行mMapView.onPause ()，实现地图生命周期管理
         mapView.onPause();
     }
 
     @Override
     protected void onResume(){
         super.onResume();
-        //在activity执行onResume时执行mMapView.onResume ()，实现地图生命周期管理
         mapView.onResume();
     }
 
@@ -253,9 +217,8 @@ public class MapActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
 
-        client.stop();//停止定位
-        baiduMap.setMyLocationEnabled(false);//关闭定位图层
-        //在activity执行onDestroy时执行mapView.onDestroy()，实现地图生命周期管理
+        client.stop();
+        baiduMap.setMyLocationEnabled(false);
         mapView.onDestroy();
         mapView=null;
     }
